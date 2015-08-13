01ac7d1c713a4e57f9e5ca8365691976196d7390
Bodil Stokke <bodil@bodil.org>
2015-08-13 15:22:48 +0100

    ES6 refresh.

--- parse.js
+++ parse.js
@@ -3,58 +3,58 @@
 type ParseResult<A> = ?[A, string];
 type Parser<A> = (input: string) => ParseResult<A>;
 
-function parse<A>(parser: Parser<A>, input: string): ParseResult<A> {
+export function parse<A>(parser: Parser<A>, input: string): ParseResult<A> {
   return parser(input);
 }
 
-function seq<A, B>(p: Parser<A>, f: (a: A) => Parser<B>): Parser<B> {
+export function seq<A, B>(p: Parser<A>, f: (a: A) => Parser<B>): Parser<B> {
   return (input) => {
-    var out = parse(p, input);
+    const out = parse(p, input);
     return out != null ? parse(f(out[0]), out[1]) : null;
   };
 }
 
-function either<A>(a: Parser<A>, b: Parser<A>): Parser<A> {
+export function either<A>(a: Parser<A>, b: Parser<A>): Parser<A> {
   return (input) => {
     return parse(a, input) || parse(b, input);
   };
 }
 
-function ret<A>(value: A): Parser<A> {
+export function ret<A>(value: A): Parser<A> {
   return (input) => [value, input];
 }
 
-function fail<A>(input: string): ParseResult<A> {
+export function fail<A>(input: string): ParseResult<A> {
   return null;
 }
 
-function item(input: string): ParseResult<string> {
+export function item(input: string): ParseResult<string> {
   return input.length > 0 ? [input[0], input.slice(1)] : null;
 }
 
-function sat(p: (c: string) => boolean): Parser<string> {
+export function sat(p: (c: string) => boolean): Parser<string> {
   return seq(item, (v) => p(v) ? ret(v) : fail);
 }
 
-var isDigit = (c) => /^\d$/.test(c);
-var isSpace = (c) => /^\s$/.test(c);
-var isAlphanum = (c) => /^\w$/.test(c);
-var isLetter = (c) => /^[a-zA-Z]$/.test(c);
-var isUpper = (c) => isLetter(c) && c == c.toUpperCase();
-var isLower = (c) => isLetter(c) && c == c.toLowerCase();
+export const isDigit = (c) => /^\d$/.test(c);
+export const isSpace = (c) => /^\s$/.test(c);
+export const isAlphanum = (c) => /^\w$/.test(c);
+export const isLetter = (c) => /^[a-zA-Z]$/.test(c);
+export const isUpper = (c) => isLetter(c) && c == c.toUpperCase();
+export const isLower = (c) => isLetter(c) && c == c.toLowerCase();
 
-var digit = sat(isDigit);
-var space = sat(isSpace);
-var alphanum = sat(isAlphanum);
-var letter = sat(isLetter);
-var upper = sat(isUpper);
-var lower = sat(isLower);
+export const digit = sat(isDigit);
+export const space = sat(isSpace);
+export const alphanum = sat(isAlphanum);
+export const letter = sat(isLetter);
+export const upper = sat(isUpper);
+export const lower = sat(isLower);
 
-function char(c: string): Parser<string> {
+export function char(c: string): Parser<string> {
   return sat((i) => i == c);
 }
 
-function string(s: string): Parser<string> {
+export function string(s: string): Parser<string> {
   if (s.length > 0) {
     return seq(char(s[0]),
            (_) => seq(string(s.slice(1)),
@@ -64,35 +64,35 @@ function string(s: string): Parser<string> {
   }
 }
 
-function manyA<A>(p: Parser<A>): Parser<Array<A>> {
+export function manyA<A>(p: Parser<A>): Parser<Array<A>> {
   return either(many1A(p), ret([]));
 }
 
-function many1A<A>(p: Parser<A>): Parser<Array<A>> {
+export function many1A<A>(p: Parser<A>): Parser<Array<A>> {
   return seq(p,
          (v) => seq(manyA(p),
          (vs) => ret([v].concat(vs))));
 }
 
-function many(p: Parser<string>): Parser<string> {
+export function many(p: Parser<string>): Parser<string> {
   return either(many1(p), ret(""));
 }
 
-function many1(p: Parser<string>): Parser<string> {
+export function many1(p: Parser<string>): Parser<string> {
   return seq(p,
          (v) => seq(many(p),
          (vs) => ret(v + vs)));
 }
 
-function str(ps: Array<Parser<string>>): Parser<string> {
+export function str(ps: Array<Parser<string>>): Parser<string> {
   return ps.length > 0 ? seq(ps[0], (v) => seq(str(ps.slice(1)), (vs) => ret(v + vs)))
       : ret("");
 }
 
-var spaces = many(space);
-var spaces1 = many1(space);
+export const spaces = many(space);
+export const spaces1 = many1(space);
 
-var num: Parser<number> = seq(
+export const num: Parser<number> = seq(
   str([
     either(char("-"), ret("")),
     many(digit),
@@ -101,55 +101,21 @@ var num: Parser<number> = seq(
       many1(digit)
     ]), ret(""))
   ]), (s) => {
-    var n = parseFloat(s);
+    const n = parseFloat(s);
     return isNaN(n) ? fail : ret(n);
   });
 
-function makeParser<A>(p: Parser<A>): ((s: string) => A) {
+export function makeParser<A>(p: Parser<A>): ((s: string) => A) {
   return (s) => {
-    var parsed = parse(p, s);
+    const parsed = parse(p, s);
     if (parsed == null) {
-      throw new Error("Syntax error in descriptor: \"" + s + "\"");
+      throw new Error(`Syntax error in descriptor: "${s}"`);
     } else {
       if (parsed[1] === "") {
         return parsed[0];
       } else {
-        throw new Error("In descriptor \"" + s + "\": expected EOF, saw \"" +
-                        parsed[1] + "\"");
+        throw new Error(`In descriptor "${s}": expected EOF, saw "${parsed[1]}"`);
       }
     }
   };
 }
-
-module.exports = {
-  parse: parse,
-  seq: seq,
-  bind: seq,
-  either: either,
-  ret: ret,
-  fail: fail,
-  item: item,
-  sat: sat,
-  digit: digit,
-  letter: letter,
-  space: space,
-  upper: upper,
-  lower: lower,
-  alphanum: alphanum,
-  char: char,
-  string: string,
-  manyA: manyA,
-  many1A: many1A,
-  many: many,
-  many1: many1,
-  str: str,
-  spaces: spaces,
-  spaces1: spaces1,
-  num: num,
-  makeParser: makeParser
-};
-
-try {
-  module.exports.ParseResult = ParseResult;
-  module.exports.Parser = Parser;
-} catch(e) {}
